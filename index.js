172116d11f0999a36ec3a0e4d3d8cb68195cef30
Brandon Lo <lobrandon1217@gmail.com>
2018-05-29 20:58:50 -0400

    new redesign incredible
    yes

--- index.js
+++ index.js
@@ -9,8 +9,57 @@ app = express();
 app.use( express.static( "./node_modules/jquery/dist/" ) );
 app.use( express.static( "./node_modules/popper.js/dist/" ) );
 
+// use json
+app.use( bodyparser.urlencoded({ extended: true }) );
+app.use( bodyparser.json() );
+
+// placeholder for plane seats
+var planeSeats = {};
+
 // www for html files
 app.use( express.static( "./www/", { extensions: ['html', 'css'] } ) );
+
+// post requests
+// simple get taken seats
+app.post("/post/get_taken_seats", function(req,res,next){
+    res.json(planeSeats);
+});
+
+// book a seat
+app.post( "/post/book_seat", function( req,res ) {
+    // we first get the data
+    let formData = req.body;
+    // first we check for values
+    if( formData.firstName.indexOf(" ") === 0 || formData.lastName.indexOf(" ") === 0 ) {
+        //check for space
+        res.json({
+            success: false,
+            error: "Names cannot begin with a space."
+        });
+        return;
+    } else if( formData.seat.length < 2 ) {
+        //check for seat
+        res.json({
+            success: false,
+            error: "You must select a seat."
+        });
+        return;
+    }
+    // check if the requested seat is in there
+    if( formData.seat in planeSeats ) {
+        res.json({
+            'success': false,
+            'error': 'This seat is already taken.'
+        });
+        return;
+    } else {
+        planeSeats[ formData.seat ] = `${formData.firstName} ${formData.middleName} ${formData.lastName}`;
+        res.json({
+            success: true
+        });
+        return;
+    }
+} );
 
 // listen
 app.listen(80, function(){
